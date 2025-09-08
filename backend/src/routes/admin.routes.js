900e7dd75d78f73dec1b788cbf4fc301cc3c7a81
AbhishekPatil1310 <ap7497493@gmail.com>
2025-09-08 18:47:31 +0530

    Add files via upload

---
+++ backend/src/routes/admin.routes.js
@@ -0,0 +1,28 @@
+const { getAllUser, getUserBymail, updateUser, deleteUser, banUser, unbanUser, sendMailToUser, addCredit, getAllAdsAnalytics,
+     getAds, deleteAd, getAffilateAds, AddAffiliateAd, updateAffiliateAd, DeleteAffiliateAd,
+    getWithdrawals,updateWithdrawalStatus, 
+    bulkUpdateWithdrawalStatus} = require('../controllers/admin.controller');
+const fp = require('fastify-plugin');
+async function adminRoutes(fastify) {
+    fastify.get('/admin/users', getAllUser);
+    fastify.get('/admin/users/search', getUserBymail);
+    fastify.put('/admin/users/:id', updateUser);
+    fastify.put('/admin/users/:id/ban', banUser);
+    fastify.delete('/admin/users/:id', deleteUser);
+    fastify.delete('/admin/ads/:id', deleteAd);
+    fastify.post('/admin/mail', sendMailToUser);
+    fastify.post('/admin/credit', addCredit);
+    fastify.get('/admin/ads/analytics', getAllAdsAnalytics);
+    fastify.get('/admin/ads/:id', getAds);
+    fastify.get('/getAffiliateAds', getAffilateAds);
+    fastify.post('/addAffiliateAd', AddAffiliateAd);
+    fastify.put('/updateAffiliateAd/:id', updateAffiliateAd);
+    fastify.delete('/deleteAffiliateAd/:id', DeleteAffiliateAd);
+    fastify.put('/admin/users/:id/unban', unbanUser);
+    fastify.patch('/withdraw/:id', updateWithdrawalStatus);
+    fastify.patch('/withdraw', bulkUpdateWithdrawalStatus);
+    fastify.get('/withdraw', getWithdrawals);
+    
+}
+
+module.exports = fp(adminRoutes);
